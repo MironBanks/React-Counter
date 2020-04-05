8bad0d8466dd53b89cb5693a3f66b0c011bb478a
Miron Banks <madapegarage@gmail.com>
2020-04-06 01:14:17 +0200

    create counter component

---
+++ src/components/CounterApp/CounterApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const CounterApp = () => {
+    return (
+        <div>
+            <h1>Im Counter App</h1>
+        </div>
+    )
+}
+
+
+
+
+export default CounterApp
